4ff2ec20eda7aef4cea07fbaeed2afcdd00a9241
rajatarora21 <arorarajat9999@gmail.com>
2019-10-07 10:56:34 -0400

    Changes made in details.html
    Collected data in details.html and used BootStrap tables to display the data

--- login/views.py
+++ login/views.py
@@ -5,4 +5,18 @@ def index(request):
     return render(request,'index.html')
 
 def details(request):
-    return render(request,'details.html')
+    name=request.POST['name'];
+    dob=request.POST['dateOfBirth']
+    occupation=request.POST['Occupation']
+    religion=request.POST['Religion'];
+    city=request.POST['City']
+    gender=request.POST['gender']
+    data={
+        'name':name,
+        'dob':dob,
+        'occupation':occupation,
+        'religion':religion,
+        'city':city,
+        'gender':gender,
+    }
+    return render(request,'details.html',data)
