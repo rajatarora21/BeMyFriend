775f6d541b4582ba5287be107a639022c10ded7b
rajatarora21 <arorarajat9999@gmail.com>
2019-10-07 19:47:59 -0400

    Changed the UI
    Changed the UI of the Project

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,4 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    path('details',views.details,name='details'),
-    path('passwordCheck',views.passwordCheck, name='passwordCheck')
 ]
