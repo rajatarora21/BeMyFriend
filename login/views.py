26b4988a322e0ce933002fb0b0a9245396b7b88f
rajatarora21 <arorarajat9999@gmail.com>
2019-10-07 10:26:38 -0400

    Dynamic website
    Making the project by recieving the data, entered by displaying the data.

--- login/views.py
+++ login/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def details(request):
+    return render(request,'details.html')
